0df1f735e80f34be94b15001172c6dff3f1392bc
Nicolai Negru <negru.n@gmail.com>
2025-11-08 19:02:46 +0000

    enums.py

---
+++ src/models/enums.py
@@ -0,0 +1,14 @@
+from enum import StrEnum
+
+
+class Title(StrEnum):
+    MR = "Mr."
+    MRS = "Mrs."
+    MS = "Ms"
+    MISS = "Miss"
+    MX = "Mx"
+    DR = "Dr."
+    SIR = "Sir"
+    DAME = "Dame"
+    LORD = "Lord"
+    LADY = "Lady"
