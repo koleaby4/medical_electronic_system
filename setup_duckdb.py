46f3c4502dc5ec47e2483d3c6cc980741ab68d8a
Nicolai Negru <negru.n@gmail.com>
2025-11-09 18:26:20 +0000

    use duckdb sequence for patient_id

--- setup_duckdb.py
+++ setup_duckdb.py
@@ -7,9 +7,12 @@ from settings import Settings
 
 def create_tables(db_file: Path):
     with duckdb.connect(db_file) as conn:
+
+        conn.execute("CREATE SEQUENCE IF NOT EXISTS patients_id_seq")
+
         conn.execute("""
              CREATE TABLE IF NOT EXISTS patients (
-                 patient_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                 patient_id INTEGER PRIMARY KEY DEFAULT nextval('patients_id_seq'),
                  title VARCHAR,
                  first_name VARCHAR,
                  middle_name VARCHAR NULL,
