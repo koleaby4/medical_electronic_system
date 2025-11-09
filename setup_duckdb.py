de4a37455375b4b560a18dff0251316c9bca0df7
Nicolai Negru <negru.n@gmail.com>
2025-11-09 16:07:58 +0000

    setup_duckdb.py

--- setup_duckdb.py
+++ setup_duckdb.py
@@ -7,12 +7,9 @@ from settings import Settings
 
 def create_tables(db_file: Path):
     with duckdb.connect(db_file) as conn:
-
-        conn.execute("CREATE SEQUENCE IF NOT EXISTS seq_patient_id START 1")
-
         conn.execute("""
              CREATE TABLE IF NOT EXISTS patients (
-                 patient_id INTEGER PRIMARY KEY default nextval('seq_patient_id'),
+                 patient_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                  title VARCHAR,
                  first_name VARCHAR,
                  middle_name VARCHAR NULL,
@@ -21,6 +18,7 @@ def create_tables(db_file: Path):
                  email VARCHAR,
                  phone VARCHAR)
                      """)
+
 
 if __name__ == "__main__":
     st = Settings()
