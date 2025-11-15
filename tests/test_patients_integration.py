82efcfc73b41b5383cca936d4aebeab7ad18ed0e
Nicolai Negru <negru.n@gmail.com>
2025-11-15 17:53:51 +0000

    sex working

--- tests/test_patients_integration.py
+++ tests/test_patients_integration.py
@@ -12,10 +12,11 @@ def test_patients_page_initially_empty(client: TestClient):
 
 def test_create_patient_and_list(client: TestClient):
     form = {
-        "title": "Mr.",
+        "title": "Mr",
         "first_name": "john",
         "middle_name": "albert",
         "last_name": "doe",
+        "sex": "male",
         "dob": "1990-01-02",
         "email": "JOHN.DOE@EXAMPLE.COM",
         "phone": "+1-555-0100",
@@ -26,8 +27,9 @@ def test_create_patient_and_list(client: TestClient):
     assert resp.status_code == 200
     html = resp.text
 
-    assert "Mr." in html
+    assert "Mr" in html
     assert "John" in html
     assert "Albert" in html
     assert "Doe" in html
+    assert "male" in html
     assert "john.doe@example.com" in html
