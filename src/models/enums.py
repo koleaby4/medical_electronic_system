43d562b2431c7f930f2756af54b330e77a283e53
Nicolai Negru <negru.n@gmail.com>
2025-11-15 17:16:58 +0000

    add Sex enum

--- src/models/enums.py
+++ src/models/enums.py
@@ -2,13 +2,19 @@ from enum import StrEnum
 
 
 class Title(StrEnum):
-    MR = "Mr."
-    MRS = "Mrs."
+    MR = "Mr"
+    MRS = "Mrs"
     MS = "Ms"
     MISS = "Miss"
     MX = "Mx"
-    DR = "Dr."
+    DR = "Dr"
     SIR = "Sir"
     DAME = "Dame"
     LORD = "Lord"
     LADY = "Lady"
+
+class Sex(StrEnum):
+    FEMALE = "female"
+    MALE = "male"
+    UNKNOWN = "unknown"
+    SPECIAL = "special"
