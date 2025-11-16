335bdbb736f3049597cce138c12c0bf4d6671165
Nicolai Negru <negru.n@gmail.com>
2025-11-16 17:14:33 +0000

    add medical_checks to DbStorage

--- src/data_access/db_storage.py
+++ src/data_access/db_storage.py
@@ -3,13 +3,17 @@ from dataclasses import dataclass
 from pathlib import Path
 
 from src.data_access.patients import DuckDbPatientsStorage
+from src.data_access.medical_checks import DuckDbMedicalChecksStorage
 
 
 @dataclass
 class DbStorage:
     def __init__(self, duckdb_file: Path) -> None:
         self.patients = DuckDbPatientsStorage(duckdb_file)
+        self.medical_checks = DuckDbMedicalChecksStorage(duckdb_file)
 
     def close(self) -> None:
         with suppress(Exception):
             self.patients.close()
+        with suppress(Exception):
+            self.medical_checks.close()
